a12a6276176c61fae9eb16f80851114bb097900f
KarloFab <fabijanickarlo1@gmail.com>
2019-11-01 21:50:17 +0100

    initial commit

---
+++ src/main/java/com/karlo/springrestdocs/SpringrestdocsApplication.java
@@ -0,0 +1,13 @@
+package com.karlo.springrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestdocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karlo/springrestdocs/SpringrestdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlo.springrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestdocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
